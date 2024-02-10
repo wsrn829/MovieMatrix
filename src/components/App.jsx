b2f721ef7cb467c6cf4efe08633f2c2bb03ae374
wsrn829 <wsrn829@gmail.com>
2024-02-10 18:31:51 -0500

    Add styling to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 
+import useStyles from './styles';
+
 import { MovieInformation, Movies, Actors, Profile, NavBar } from '.';
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <NavBar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
           <Route path="/movie/:id" element={<MovieInformation />} />
           <Route path="/actors/:id" element={<Actors />} />
