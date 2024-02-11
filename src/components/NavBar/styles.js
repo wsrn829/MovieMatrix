8581c468bee16084d0d1ef7bb5ce784f5ed7d0d6
wsrn829 <wsrn829@gmail.com>
2024-02-10 23:46:42 -0500

    Complete navbar layout

---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles((theme) => ({
+    toolbar: {
+        height: '80px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginLeft: '240px',
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+}));
